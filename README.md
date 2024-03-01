28c973bdbe4d9c530fa3eab02c83169e5a8767a3
Jacky Woo <146035892+jackywooks@users.noreply.github.com>
2024-03-01 18:42:30 -0500

    add UI for the game

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rock-Paper-Scissors
 Version 1:
     a rock paper scissor game that play by accepting input from the message box, and the result is shown in the console.
+Version 2:
+    add GUI
--- script/function.js
+++ script/function.js
@@ -1,54 +1,88 @@
 // write a function to randomly generate computer's choice of rock paper and scissor, return the randomly generated choice
 const choiceList = ["ROCK", "PAPER", "SCISSOR"];
 
-function getComputerChoice() {
+//initiate the score
+let computerScore = 0;
+let playerScore = 0;
+let gameNo = 0;
+
+//DOM Manipulation
+//declare the element's varaibles
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorButton = document.querySelector("#scissor");
+//create paragraph to display result
+const result = document.querySelector("#result");
+const playerChoicePara = document.createElement("p");
+const computerChoicePara = document.createElement("p");
+const roundResultPara = document.createElement("p");
+const runningScorePara = document.createElement("p");
+result.append(
+  playerChoicePara,
+  computerChoicePara,
+  roundResultPara,
+  runningScorePara
+);
+
+//add event listeners to the buttons to start the game
+rockButton.addEventListener("click", () =>
+  playRound("ROCK", getcomputerChoicePara())
+);
+paperButton.addEventListener("click", () =>
+  playRound("PAPER", getcomputerChoicePara())
+);
+scissorButton.addEventListener("click", () =>
+  playRound("SCISSOR", getcomputerChoicePara())
+);
+
+//get a random choice from computer
+function getcomputerChoicePara() {
   //use array to store the computer's choice
-  const cpuChoice = Math.floor(Math.random() * choiceList.length);
-  return choiceList[cpuChoice];
+  const computerChoice = Math.floor(Math.random() * choiceList.length);
+  return choiceList[computerChoice];
 }
 
 //write a function to play the round
-function playRound(playerSelection, computerSelection) {
+function playRound(playerChoice, computerChoice) {
   //change the selection of both players and computer to upper case to handle case sensitivity.
-  playerSelection = playerSelection.toUpperCase();
-  console.log("Player: ", playerSelection);
-  console.log("Computer: ", computerSelection);
+  playerChoice = playerChoice.toUpperCase();
+  playerChoicePara.textContent = `Player: ${playerChoice}`;
+  computerChoicePara.textContent = `Computer: ${computerChoice}`;
   let result = 0;
   //compare the rock paper scissor and determine winner, if else statement
-  if (playerSelection === computerSelection) {
+  if (playerChoice === computerChoice) {
+    playGame("Tie");
     return "Tie";
   } else if (
-    (playerSelection === "ROCK" && computerSelection === "SCISSOR") ||
-    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
-    (playerSelection === "SCISSOR" && computerSelection === "PAPER")
+    (playerChoice === "ROCK" && computerChoice === "SCISSOR") ||
+    (playerChoice === "PAPER" && computerChoice === "ROCK") ||
+    (playerChoice === "SCISSOR" && computerChoice === "PAPER")
   ) {
+    playGame("Player Wins");
     return "Player Wins";
   } else {
+    playGame("Player Loses");
     return "Player Loses";
   }
 }
 
-//write a function to play game = 5rounds
-function playGame() {
-  playerscore = 0;
-  computerscore=0;
-  for (i = 0; i < 5; i++) {
-    playerSelection = prompt("Please input 'Rock','Paper' or 'Scissor'");
-    if (choiceList.includes(playerSelection.toUpperCase())) {
-      result = playRound(playerSelection, getComputerChoice());
-      console.log(result);
-      if (result === "Player Wins") {
-        playerscore++;
-      } else if (result === "Player Loses"){
-        computerscore++;
-      }
-      console.log("Player Score", playerscore,"Computer Score", computerscore);
-    } else {
-      console.log("Invalid Input");
-      break;
-    }
+//write a function to calculate the score of a game = 5rounds
+function playGame(roundResult) {
+  if (roundResult === "Player Wins") {
+    playerScore++;
+  } else if (roundResult === "Player Loses") {
+    computerScore++;
   }
-  console.log("Your Final Score:", playerscore," vs Computer",computerscore);
+  roundResultPara.textContent = roundResult;
+  // stay in the game if either player is less than 5 points
+  if (playerScore < 5 && computerScore < 5) {
+    runningScorePara.textContent = `PlayerScore: ${playerScore} Computer Score: ${computerScore}`;
+  } else {
+    gameNo ++;
+    const finalResult = document.createElement("p");
+    finalResult.textContent = playerScore==5?`Round ${gameNo} Player Wins.`:`Round ${gameNo} Computer Wins`;
+    result.append(finalResult);
+    playerScore = 0;
+    computerScore = 0;
+  }
 }
-
-playGame();
